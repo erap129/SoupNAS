385b44770aae962a339a1fe252441c72a5441813
erap129 <eladr@post.bgu.ac.il>
2020-03-26 10:50:21 +0200

    added paths experiment

--- SoupNAS.py
+++ SoupNAS.py
@@ -19,7 +19,6 @@ from datetime import datetime
 from sacred import Experiment
 import pandas as pd
 import sys
-import argparse
 
 
 ex = Experiment()
@@ -127,7 +126,7 @@ class OptionsSoupNetwork(nn.Module):
 
 
 class LinearSoupNetwork(nn.Module):
-    def __init__(self, layer_stock, soup_size, n_blocks, block_size, random_input_factor, num_receptors):
+    def __init__(self, layer_stock, soup_size, n_blocks, block_size, random_input_factor, num_receptors, paths):
         super(LinearSoupNetwork, self).__init__()
         self.layer_stock = layer_stock
         self.soup_size = soup_size
@@ -136,6 +135,7 @@ class LinearSoupNetwork(nn.Module):
         self.num_receptors = num_receptors
         self.random_input_factor = random_input_factor
         self.expansion_convs = ModuleList([Conv2dAuto(in_channels=max(1, 32 * i), out_channels=32 * (i + 1), kernel_size=3) for i in range(n_blocks)])
+        self.paths = paths
         self.soup = self.make_linear_soup()
         self.linear = nn.Linear(int(x_train.shape[2] / 2 ** n_blocks) * int(x_train.shape[3] / 2 ** n_blocks) * int(32 * (n_blocks)), 10)
 
@@ -167,9 +167,18 @@ class LinearSoupNetwork(nn.Module):
                 if layer.num_receptors > 1:
                     soup[str(block_idx)][f'{i}_{prev_layers}_connector'] = \
                         ConcatThenOneByOne(block_channels, layer.num_receptors)
+        block_channels += 32
+        if self.paths > 1:
+            soup['final_connector'] = ConcatThenOneByOne(block_channels, self.paths)
         return soup
 
     def forward(self, X):
+        if self.paths == 1:
+            return self.forward_regular(X)
+        else:
+            return self.forward_paths(X)
+
+    def forward_regular(self, X):
         run_str = 'input->'
         for block_idx in range(self.n_blocks):
             block_outputs = {}
@@ -177,17 +186,20 @@ class LinearSoupNetwork(nn.Module):
             run_str += f'BLOCK_{block_idx}['
             for step in range(self.block_size):
                 relevant_layers = {k: v for k, v in self.soup[str(block_idx)].items() if
-                                   'connector' not in k and X.tag in intlst_to_strlst(ast.literal_eval(k.split('_')[1]))}
+                                   'connector' not in k and X.tag in intlst_to_strlst(
+                                       ast.literal_eval(k.split('_')[1]))}
                 if len(relevant_layers) > 0:
                     layer_key = random.choice(list(relevant_layers.keys()))
                     layer = self.soup[str(block_idx)][layer_key]
                     if layer.num_receptors == 1:
                         X = layer(X)
                     else:
-                        relevant_inputs = [v for k, v in block_outputs.items() if k != X.tag and k in intlst_to_strlst(ast.literal_eval(layer_key.split('_')[1]))]
+                        relevant_inputs = [v for k, v in block_outputs.items() if k != X.tag and k in intlst_to_strlst(
+                            ast.literal_eval(layer_key.split('_')[1]))]
                         if len(relevant_inputs) >= layer.num_receptors - 1:
                             inputs = [X] + random.sample([v for k, v in block_outputs.items() if k != X.tag and
-                                                          k in intlst_to_strlst(ast.literal_eval(layer_key.split('_')[1]))],
+                                                          k in intlst_to_strlst(
+                                ast.literal_eval(layer_key.split('_')[1]))],
                                                          layer.num_receptors - 1)
                             X = self.soup[str(block_idx)][f'{layer_key}_connector'](*inputs)
                         else:
@@ -200,7 +212,46 @@ class LinearSoupNetwork(nn.Module):
             X = self.expansion_convs[block_idx](X)
             X = nn.MaxPool2d(2, stride=2)(X)
             X.tag = save_tag
-        # print(run_str)
+        X = X.view(X.size(0), -1)
+        X = self.linear(X)
+        X = nn.functional.softmax(X)
+        return X
+
+    def forward_paths(self, X):
+        for block_idx in range(self.n_blocks):
+            X.tag = '-1'
+            block_outputs = [{X.tag: X} for i in range(self.paths)]
+            for step in range(self.block_size):
+                relevant_layers = {}
+                for i in range(len(block_outputs)):
+                    relevant_layers[i] = {k: v for k, v in self.soup[str(block_idx)].items() if
+                                        'connector' not in k and list(block_outputs[i].keys())[0] in intlst_to_strlst(ast.literal_eval(k.split('_')[1]))}
+                relevant_layers = {k: v for k, v in relevant_layers.items() if len(v) > 0}
+                if len(relevant_layers) > 0:
+                    chosen_path = random.choice(list(relevant_layers.keys()))
+                    layer_key = random.choice(list(relevant_layers[chosen_path].keys()))
+                    layer = self.soup[str(block_idx)][layer_key]
+                    if layer.num_receptors == 1:
+                        X = layer(list(block_outputs[chosen_path].values())[0])
+                    else:
+                        relevant_inputs = []
+                        for i in range(len(block_outputs)):
+                            relevant_inputs.extend([v for k, v in block_outputs[i].items() if k != X.tag and k in intlst_to_strlst(ast.literal_eval(layer_key.split('_')[1]))])
+                        if len(relevant_inputs) >= layer.num_receptors - 1:
+                            inputs = [X] + random.sample(relevant_inputs, layer.num_receptors - 1)
+                            X = self.soup[str(block_idx)][f'{layer_key}_connector'](*inputs)
+                        else:
+                            X = layer(X)
+                    X.tag = layer_key.split('_')[0]
+                    block_outputs[chosen_path] = {X.tag: X}
+            for i in range(len(block_outputs)):
+                X = list(block_outputs[i].values())[0]
+                save_tag = X.tag
+                X = self.expansion_convs[block_idx](X)
+                X = nn.MaxPool2d(2, stride=2)(X)
+                X.tag = save_tag
+                block_outputs[i] = {X.tag: X}
+        X = self.soup['final_connector'](*[list(b.values())[0] for b in block_outputs])
         X = X.view(X.size(0), -1)
         X = self.linear(X)
         X = nn.functional.softmax(X)
@@ -297,6 +348,33 @@ def skip_linear_experiment(callbacks, today_str):
                 pd.DataFrame(dict_list).to_csv(f'results/{today_str}/skip_linear_experiment.csv')
 
 
+def paths_experiment(callbacks, today_str):
+    dict_list = []
+    for paths in range(1, 10):
+        for iteration in range(100):
+            skorch_sn = NeuralNetClassifier(
+                module=LinearSoupNetwork,
+                module__layer_stock=layer_stock,
+                module__soup_size=30,
+                module__n_blocks=3,
+                module__block_size=10,
+                module__random_input_factor=[[1 for i in range(10)] for j in range(3)],
+                module__num_receptors=[[1 for i in range(10)] for j in range(3)],
+                module__paths=paths,
+                max_epochs=50,
+                lr=0.1,
+                iterator_train__shuffle=True,
+                device='cuda',
+                callbacks=callbacks
+            )
+        skorch_sn.fit(fmnist_train.train_data[:, None, :, :].float(), fmnist_train.train_labels.long())
+        y_pred = skorch_sn.predict(fmnist_test.test_data[:, None, :, :].float())
+        accuracy = metrics.accuracy_score(fmnist_test.test_labels.long(), y_pred)
+        dict_list.append({'paths': paths,
+                          'iteration': iteration,
+                          'accuracy': accuracy})
+        pd.DataFrame(dict_list).to_csv(f'results/{today_str}/paths_experiment.csv')
+
 
 def options_experiment(callbacks, today_str):
     dict_list = []
@@ -344,6 +422,8 @@ def my_main():
         skip_experiment(callbacks, today_str)
     elif exp == 'skip_linear':
         skip_linear_experiment(callbacks, today_str)
+    elif exp == 'paths':
+        paths_experiment(callbacks, today_str)
     filename = 'SoupNAS_fashion_mnist.csv'
     pd.DataFrame(dict_list).to_csv(filename)
     ex.add_artifact(filename)
